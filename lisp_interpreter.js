a87f550cdae24b2ae367b5fcbea2557f0fb8c1c0
Sharan Ram <sharanram1993@gmail.com>
2017-02-17 13:07:55 +0530

    First Commit

---
+++ lisp_interpreter.js
@@ -0,0 +1,73 @@
+
+function tokenize(input) {
+    return input.replace(/\(/g," ( ").replace(/\)/g," ) ").trim().split(/ +/);
+}
+
+function createArr(tokens,list=[]) {
+
+    var token = tokens.shift();
+    if(token === "(") {
+        var list = [];
+
+        while(tokens[0]!== ")") {
+            list.push(createArr(tokens));
+        }
+
+    tokens.shift();
+    return list;
+    }
+
+    else if(token === ")") {
+        return "syntax error";
+    }
+
+    return catogarize(token);
+}
+
+function catogarize(token) {
+    if(!isNaN(float=parseFloat(token))) {
+        return float;
+    }
+    return token;
+}
+
+function parse(input) {
+    return createArr(tokenize(input));
+}
+
+
+//console.log(parse(`((lambda (x) x) "lisp")`));
+//console.log(parse(`(define a 5)`));
+
+
+var library = {
+    '+':function(a,b) {return a+b},
+    '-':function(a,b) {return a-b},
+    '*':function(a,b) {return a*b},
+    '/':function(a,b) {return a/b},
+    '%':function(a,b) {return a%b},
+    '>':function(a,b) {return a>b},
+    '<':function(a,b) {return a<b},
+    '>=':function(a,b) {return a>=b},
+    '<=':function(a,b) {return a<=b},
+    'abs':function(a,b=null) {return Math.abs(a)},
+    //'append':function(a,b) {return String(a)+String(b)},
+    //'car':function(a) {return a[0]},
+    //'cdr':function(a,b) {return a.slice(1)},
+    'eq?':function(a,b) {return a==b},
+    'equal?':function(a,b) {return a===b},
+    //'cons':function(a,b) {a.push(b);return a},
+    //'length':function(a) {return a.length},
+    'max':function(a,b) {return Math.max(a,b)},
+    'min':function(a,b) {return Math.min(a,b)},
+    'not':function(a,b=null) {return !a},
+    'number?':function(a) {return !isNaN(a)},
+    // 'pi': function() {return Math.PI();}
+}
+
+
+function eval(input) {
+    return library[input[0]](input[1],input[2]);
+}
+
+console.log(eval(parse((`(+ 1 2)`))));
